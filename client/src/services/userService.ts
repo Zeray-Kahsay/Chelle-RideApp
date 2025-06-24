400d0c8fae560cb98a27f0d1946a683395972850
Zeray-Kahsay <kahsayzeray@gmail.com>
2025-06-24 03:54:03 +0200

    unneccessary folders deleted

--- client/src/services/userService.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { registerUser } from "../api/userApi";
-import type { RegisterUserRequest } from "../features/Account/types/RegisterUserRequest";
-
-export const useRegisterUser = () => {
-  return useMutation({
-    mutationFn: (data: RegisterUserRequest) => registerUser(data),
-  });
-};
